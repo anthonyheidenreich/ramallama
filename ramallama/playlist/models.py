f0e8378d9d6df486eb635525f6f97155dfad1bff
Rui Nakata <rui@wpengine.com>
2016-08-21 17:59:25 -0500

    added artist model

--- ramallama/playlist/models.py
+++ ramallama/playlist/models.py
@@ -17,8 +17,7 @@ class Playlist(models.Model):
 
 class Song(models.Model):
     title = models.CharField(max_length=200)
-    artist = models.CharField(max_length=100)
-    year = models.CharField(max_length=10)
+    year = models.CharField(max_length=10, blank=True)
     playlists = models.ManyToManyField(Playlist)
     created_on = models.DateTimeField(auto_now_add=True) 
     updated_on = models.DateTimeField(auto_now=True)
@@ -30,7 +29,7 @@ class Song(models.Model):
 class SongSource(models.Model):
     source = models.CharField(choices=SOURCE_CHOICES, max_length=20)
     external_id = models.CharField(max_length=100)
-    preview_url = models.CharField(max_length=200)
+    preview_url = models.CharField(max_length=200, blank=True)
     created_on = models.DateTimeField(auto_now_add=True) 
     updated_on = models.DateTimeField(auto_now=True)
     def __str__(self):
@@ -38,17 +37,21 @@ class SongSource(models.Model):
     class Meta:
         ordering = ('created_on',)
 
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+    songs = models.ManyToManyField(Song)
+    created_on = models.DateTimeField(auto_now_add=True) 
+    updated_on = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ('created_on',)
 
+class ArtistSource(models.Model):
+    source = models.CharField(choices=SOURCE_CHOICES, max_length=20)
+    external_id = models.CharField(max_length=100)
+    created_on = models.DateTimeField(auto_now_add=True) 
+    updated_on = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.external_id
+    class Meta:
+        ordering = ('created_on',)
 
-
-# class Artist(models.Model):
-#     name = models.CharField(max_length=100)
-#     created_on = models.DateTimeField(auto_now_add=True) 
-#     updated_on = models.DateTimeField(auto_now=True)
-
-#     class Meta:
-#         ordering = ('created_on',)
-
-# class ArtistSong(models.Model):
-
-# class ArtistSource()
---
+++ ramallama/playlist/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from playlist.models import Playlist, Song, SongSource
+
+
+class PlaylistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Playlist
+        fields = ('name', 'external_id', 'source', 'created_on', 'updated_on')
+
+class SongSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Song
+        fields = ('title', 'artist', 'year', 'playlists', 'created_on', 'updated_on')
+
+class SongSourceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SongSource
+        fields = ('source', 'external_id', 'preview_url', 'created_on', 'updated_on')
