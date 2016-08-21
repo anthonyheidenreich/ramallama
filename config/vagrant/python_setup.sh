457568eb6357232ecab34ff881a1b517402fe997
Anthony Heidenreich <Anthony.Heidenreich@allclearid.com>
2016-08-21 14:20:14 -0500

    working python vagrant

---
+++ config/vagrant/python_setup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo "=== Begin Vagrant Provisioning using 'config/vagrant/python_setup.sh'"
+PYTHON_VERSION='3.5.2'
+sudo add-apt-repository ppa:fkrull/deadsnakes
+sudo apt-get -y update
+sudo apt-get install -y python3.5 python3.5-dev python3-pip
+
+cd ~
+
+sudo pip3 install --upgrade pip
+sudo pip3 install virtualenv
+
+virtualenv -p /usr/bin/python3.5 venv
+. venv/bin/activate
+
+pip3 install -r /vagrant/requirements.txt
+
+deactivate
