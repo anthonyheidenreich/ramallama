6cdbde75442ebdc75fc8e9fb56f5e07bac866727
Anthony Heidenreich <Anthony.Heidenreich@allclearid.com>
2016-08-28 15:25:50 -0500

    remove extra print

--- ramallama/ramallama/settings.py
+++ ramallama/ramallama/settings.py
@@ -80,9 +80,6 @@ DATABASES = {
     'default': dj_database_url.config()
 }
 
-print(DATABASES)
-
-
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
