c339048f8664273755ede53dbdf482cdb0950185
Anthony Heidenreich <Anthony.Heidenreich@allclearid.com>
2016-09-05 17:02:36 -0500

    refactored bootstrap

--- bootstrap.py
+++ bootstrap.py
@@ -3,133 +3,128 @@ import argparse
 import requests
 
 
-class Bootstrap():
-    commands = {}
-
-    @staticmethod
-    def execute(command, args):
-        try:
-            fake = Factory.create()
-            return Bootstrap.commands[command](fake, args.number)
-        except Exception as e:
-            raise e
-
-    @staticmethod
-    def available():
-        return Bootstrap.commands.keys()
-
-
+COMMANDS = {}
 def command(func):
-    Bootstrap.commands[func.__name__] = func
+    COMMANDS[func.__name__] = func
     return func
 
 
-@command
-def playlists(fake, number=1):
-    """
-    Create N playlists
-    """
-    created = []
-    for _ in range(0, number):
-        created.append(create_playlist(fake))
-    return created
+class Bootstrap():
+
+    def __init__(self, url):
+        self.faker = Factory.create()
+        self.url = url
+
+    def _post(self, uri, json={}):
+        r = requests.post('{}/{}'.format(self.url, uri), json=json)
+        return r.json()
+
+
+    @command
+    def playlists(self, number=1):
+        """
+        Create N playlists
+        """
+        created = []
+        for _ in range(0, number):
+            created.append(self.create_playlist())
+        return created
 
 
 
-def create_playlist(fake):
-    """
-    Creates a single playlist
-    """
-    pl = {
-        'name': fake.sentence(nb_words=4),
-        'external_id': fake.uuid4(),
-        'source': 'Spotify',
-    }
-    r = requests.post('http://127.0.0.1/v1/playlists', json=pl)
-    return r.json()
+    def create_playlist(self):
+        """
+        Creates a single playlist
+        """
+        pl = {
+            'name': self.faker.sentence(nb_words=4),
+            'external_id': self.faker.uuid4(),
+            'source': 'Spotify',
+        }
+        return self._post('playlists', json=pl)
 
 
-@command
-def songs(fake, number=1):
-    """
-    Create N Songs with Sources
-    """
-    created = []
-    for _ in range(0, number):
-        created.append(create_song(fake))
-    return created
+    @command
+    def songs(self, number=1):
+        """
+        Create N Songs with Sources
+        """
+        created = []
+        for _ in range(0, number):
+            created.append(create_song(self))
+        return created
 
 
 
-def create_song(fake):
-    """
-    Create a single Song with Artist and Source
-    """
-    artist_obj = create_artist(fake)
+    def create_song(self):
+        """
+        Create a single Song with Artist and Source
+        """
+        artist_obj = create_artist(self)
 
-    source = {
-        'source': 'Spotify',
-        'external_id': fake.uuid4(),
-    }
-    song = {
-        'title': fake.sentence(nb_words=4),
-        'year': fake.year(),
-        'artist': artist_obj['id']
-    }
-    r = requests.post('http://127.0.0.1/v1/songs', json=song)
-    song_response = r.json()
-    source['song'] = song_response.get('id');
-    r = requests.post('http://127.0.0.1/v1/song-sources', json=source)
-    return song_response
-
-
-@command
-def artists(fake, number=1):
-    """
-    Create N Artists with Sources
-    """
-    created = []
-    for _ in range(0, number):
-        created.append(create_artist(fake))
-    return created
+        source = {
+            'source': 'Spotify',
+            'external_id': self.faker.uuid4(),
+        }
+        song = {
+            'title': self.faker.sentence(nb_words=4),
+            'year': self.faker.year(),
+            'artist': artist_obj['id']
+        }
+        song_response = self._post('songs', json=song)
+        source['song'] = song_response.get('id');
+        song_source = self._post('song-sources', json=source)
+        return song_response
 
 
-def create_artist(fake):
-    """
-    Create a single Artist with Artist Source
-    """
-    source = {
-        'source': 'Spotify',
-        'external_id': fake.uuid4(),
-    }
-    artist = {
-        'name': fake.name()
-    }
-    r = requests.post('http://127.0.0.1/v1/artists', json=artist)
-    artist_response = r.json()
-    source['artist'] = artist_response.get('id');
-    r = requests.post('http://127.0.0.1/v1/artist-sources', json=source)
-    return artist_response
+    @command
+    def artists(self, number=1):
+        """
+        Create N Artists with Sources
+        """
+        created = []
+        for _ in range(0, number):
+            created.append(create_artist(self))
+        return created
 
-@command
-def playlist_with_songs(fake, playlist_count=1):
-    """
-    Create playlist(s) with some songs
-    """
-    song_count = 5
-    for _ in range(0, playlist_count):
-        playlist = create_playlist(fake)
-        song_list = songs(fake, song_count)
-        for song in song_list:
-            r = requests.post('http://127.0.0.1/v1/playlists/{}/songs'.format(playlist.get('id')), json={'song': song.get('id')})
-            print(r.text)
+
+    def create_artist(self):
+        """
+        Create a single Artist with Artist Source
+        """
+        source = {
+            'source': 'Spotify',
+            'external_id': self.faker.uuid4(),
+        }
+        artist = {
+            'name': self.faker.name()
+        }
+        artist_response = self._post('artists', json=artist)
+        source['artist'] = artist_response.get('id');
+        self._post('artist-sources', json=source)
+        return artist_response
+
+
+    @command
+    def playlist_with_songs(self, playlist_count=1):
+        """
+        Create playlist(s) with some songs
+        """
+        song_count = 5
+        for _ in range(0, playlist_count):
+            playlist = create_playlist(self)
+            song_list = songs(self, song_count)
+            for song in song_list:
+                r = self._post('playlists/{}/songs'.format(playlist.get('id')), json={'song': song.get('id')})
+                print(r)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Populate data in the APIs')
-    parser.add_argument('command', type=str, help='API type for which to generate data. Limit to: {}'.format(', '.join(Bootstrap.available())))
+    parser.add_argument('command', type=str, help='API type for which to generate data. Limit to: {}'.format(', '.join(COMMANDS.keys())))
+    parser.add_argument('-u', '--url', dest='url', type=str, default='http://127.0.0.1/v1', help='number of records to create')
     parser.add_argument('-n', '--number', dest='number', type=int, default=1, help='number of records to create')
     args = parser.parse_args()
 
-    bootstrap = Bootstrap()
-    print(bootstrap.execute(args.command, args))
+    bootstrap = Bootstrap(args.url)
+    print(getattr(bootstrap, args.command)(args.number))
