243150feee11619fd2c4e6f66aacd5245d5b30e5
Rui Nakata <rui@wpengine.com>
2016-09-05 12:07:18 -0500

    fix nginx_setup

--- config/vagrant/nginx_setup.sh
+++ config/vagrant/nginx_setup.sh
@@ -3,6 +3,6 @@
 basedir = `dirname $0`
 echo "copy nginx config into place"
 echo "$basedir/nginx.conf /etc/nginx/sites-enabled/default"
-cp $basedir/nginx.conf /etc/nginx/sites-enabled/default
+cp nginx.conf /etc/nginx/sites-enabled/default
 
 service nginx restart
