bcc20312ecd8efc3ff36a862e2c0c3459cacecf3
Anthony Heidenreich <Anthony.Heidenreich@allclearid.com>
2016-09-06 19:28:21 -0500

    made the app work at root

--- ramallama/ramallama/settings.py
+++ ramallama/ramallama/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'ramallama.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['ramallama/playlist/static/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
